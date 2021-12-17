6098e5149cf5350727b3c6c8a2da1f4d0ccf10ca
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2021-12-17 13:57:39 -0600

    made updates on main branch to source code. next time make them in develop branch

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// JAVASCRIPT CODING QUIZ 
+
+// AS A BOOTCAMP STUDENT 
+// I WANT TO TAKE A TIMED QUIZ ON JAVASCRIP FUNDAMENTALS THAT STORES HIGH SCORES
+// SO THAT i CAN GAUGE MY PROGRESS COMPARED TO MY PEERS.
+
+// Criteria:
+// GIVEN:   I am taking a coding quiz
+// WHEN:    I click the start button
+// THEN:    A timer starts and I am presented with a question.
+//
+// WHEN:    I answer a question (correctly)
+// THEN:    I am presented with another question.
+// 
+// WHEN:    I answer a question incorrectly
+// THEN:    [Additional] time is subtracted from the clock 
+// 
+// WHEN:    All questions are answered OR the timer reaches 0
+// THEN:    The game is over!
+//
+// WHEN:    The game is over
+// THEN:    I save my score with my initials, which will then be displayed for all to see.
+
+//  user presses start button
+    //  timer begins to tick down (time interval) AND question is presented
+//  (*options are displayed for user to choose from
+//  user selects an option
+//  if correct
+    //  congratulate and move on to next question
+//  if incorrect, inform incorrect, 
+    //  subtract time from timer as pentalty and then move on to next question
+// process repeats*)
+//  after all questions are answered OR timer runs out
+    // "Game over!"" message displays
+    // when (questionsAnswered == 
+// user enters initials
+    // score is saved
+    // all scores are returned and displayed
