f2f4a6a94b09d9eefc12e910e8b2820b178c8bfc
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2021-12-18 19:53:56 -0600

    creating functions for quiz questions and modifying html

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Timed quiz on JS fundamentals
 [Displaying a message after a timer completes](https://stackoverflow.com/questions/5083534/how-do-you-display-a-message-once-a-javascript-function-restarts) as suggested by [Dominic Tancredi](https://stackoverflow.com/users/1247659/dominic-tancredi) on Stack Overflow from February 2011.
 
 
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_setinterval3
+
+https://www.elated.com/javascript-timers-with-settimeout-and-setinterval/
+https://stackoverflow.com/questions/4435776/simple-clock-that-counts-down-from-30-seconds-and-executes-a-function-afterward
+https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+https://courses.bootcampspot.com/courses/1215/pages/5-dot-5-4-schedule-task-auditing?module_item_id=367554
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,15 @@
-// JAVASCRIPT CODING QUIZ 
+//  CODING QUIZ QUESTIONS
 
-// AS A BOOTCAMP STUDENT 
-// I WANT TO TAKE A TIMED QUIZ ON JAVASCRIP FUNDAMENTALS THAT STORES HIGH SCORES
-// SO THAT i CAN GAUGE MY PROGRESS COMPARED TO MY PEERS.
+// Start Quiz button from index.html brings to quiz.html where timer starts and questions initiate. use btnStartEl for event listener at bottom of page.
+    var btnStartEl = document.querySelector('btnStart');
+// Timer begins at a set time, and counts down to zero. Penalties from wrong guesses deduct an additional 10 seconds from the clock.
+    var timeRemaining = 75;
+    var timerElement = document.getElementById('timerdiv');    
+    var timerId = setInterval(countdown, 1000);
+ 
+// Display questions one at a time. User selects answer from options a, b, c, d. Whether answer is correct or not, questions advance. Incorrect answers receive penalty in reduction of allotted time by 10 seconds.
 
-// Criteria:
-// GIVEN:   I am taking a coding quiz
-// WHEN:    I click the start button
-// THEN:    A timer starts and I am presented with a question.
-//
-// WHEN:    I answer a question (correctly)
-// THEN:    I am presented with another question.
-// 
-// WHEN:    I answer a question incorrectly
-// THEN:    [Additional] time is subtracted from the clock 
-// 
-// WHEN:    All questions are answered OR the timer reaches 0
-// THEN:    The game is over!
-//
-// WHEN:    The game is over
-// THEN:    I save my score with my initials, which will then be displayed for all to see.
 
-//  user presses start button
-    //  timer begins to tick down (time interval) AND question is presented
 //  (*options are displayed for user to choose from
 //  user selects an option
 //  if correct
@@ -36,3 +23,88 @@
 // user enters initials
     // score is saved
     // all scores are returned and displayed
+
+
+
+   
+
+    // var btnOption1El = document.getElementById('btn-option1');
+    // var btnOption2El = document.getElementById('btn-option2');
+    // var btnOption3El = document.getElementById('btn-option3');
+    // var btnOption4El = document.getElementById('btn-option4');
+
+
+    function startQuiz(event) {
+        var q = myQuestions[index];
+        myQuestions.innerHTML= q.question;
+
+        if (option != correctAnswer) {
+            penalty();
+        };
+        return askQuestion();
+    };
+
+    var myQuestions = [ 
+        {
+            questionNumber: "1",
+            question: "Inside the HTML document, where do you place your JavaScript code?",
+            option: {
+                a: "Inside the <link> element",
+                b: "Inside the <script> element",
+                c: "Inside the <head> element",
+                d: "In the <footer> element"
+          },
+          correctAnswer: "d"
+        },
+        {
+            questionNumber: "2",
+            question: "What operator is used to assign a value to a declared variable?",
+            option: {
+                a: "Question mark (?)",
+                b:"Double-equal (==)",
+                c: "Equal sign (=)",
+                d: "Colon (:)"
+          },
+          correctAnswer: "c"
+        },
+        {
+            questionNumber: "3",
+            question: "What are the six primitive data types in JavaScript?",
+            option: {
+                a:  "sentence, float, data, bigInt, symbol, undefined",
+                b: "string, num, falsy, bigInt, symbol, undefined",
+                c:  "sentence, int, truthy, bigInt, symbol, undefined",
+                d: "string, number, boolean, bigInt, symbol, undefined",
+            },
+            correctAnswer: "d"
+        },
+        {
+            questionNumber: "4",
+            question: "What is the difference between && and ||? *",
+            option: {
+                a: "The logical operator && returns true if none of the expressions are true while the logical operator || returns true if one expression or the other returns true.",
+                b: "The logical operator && returns true if both expressions are true while the logical operator || returns false if one expression or the other returns true.",
+                c: "The logical operator && returns true if one expression is true while the logical operator || returns true if both expressions returntrue true.",
+                d: "The logical operator && returns true if both expressions are true while the logical operator || returns true if one expression or the other returns true.",
+            },
+            correctAnswer: "d"
+          }
+      ];
+   
+    function countdown() {
+    if (timeRemaining == -1) {
+        clearTimeout(timerId);
+        alert('Game Over!');
+      } else {
+        timerElement.innerHTML = timeRemaining;
+        timeRemaining--;
+      }
+    }
+
+    var penalty = function() {
+        if (selectedOption != correctAnswer){
+        timeRemaining - 10;
+        }
+    }
+
+btnStartEl.addEventListener('click', startQuiz, countdown);
